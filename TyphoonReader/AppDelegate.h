24442293ea7382fcf0a0ea201603d22c1398ed0f
岩田裕也 <yuya@iwatayuuya-no-MacBook-Pro.local>
2014-10-15 23:31:46 +0900

    Initial Commit

---
+++ TyphoonReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TyphoonReader
+//
+//  Created by 岩田裕也 on 2014/10/15.
+//  Copyright (c) 2014年 Yuya IWATA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TyphoonReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TyphoonReader
+//
+//  Created by 岩田裕也 on 2014/10/15.
+//  Copyright (c) 2014年 Yuya IWATA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
